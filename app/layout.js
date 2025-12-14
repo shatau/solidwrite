d90a52a447e51d31a1cb0c9c3728a8b11d998437
shatau <shalala.adilova@gmail.com>
2025-12-14 19:40:22 +0100

    datafast

--- app/layout.js
+++ app/layout.js
@@ -18,11 +18,11 @@ export const viewport = {
 export async function generateMetadata() {
 
 	return getSEOTags({
-	  title: "AI Humanizer by Solid Write: Bypass AI Detectors ",
-	  description: "Transform AI-generated text into natural, human-like content that bypasses AI detection.",
-	  canonicalUrlRelative: "http://localhost:3000"
+		title: "AI Humanizer by Solid Write: Bypass AI Detectors ",
+		description: "Transform AI-generated text into natural, human-like content that bypasses AI detection.",
+		canonicalUrlRelative: "http://localhost:3000"
 	});
-  }
+}
 
 export default function RootLayout({ children }) {
 	return (
@@ -31,6 +31,15 @@ export default function RootLayout({ children }) {
 			data-theme={config.colors.theme}
 			className={font.className}
 		>
+
+			<head>
+				<script
+					defer
+					data-website-id="dfid_P8rU79Du8bSylUKN5Qnl0"
+					data-domain="solidwrite.com"
+					src="https://datafa.st/js/script.js">
+				</script>
+			</head>
 			<body>
 				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
 				<ClientLayout>{children}</ClientLayout>
